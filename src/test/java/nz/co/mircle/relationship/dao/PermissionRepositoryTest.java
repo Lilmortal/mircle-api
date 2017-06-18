b50f9129675807bc64100d7f7f17735450099acf
Jack Tan <jacktan@Jacks-iMac.local>
2017-06-19 00:12:19 +1200

    Added relationship controller test

---
+++ src/test/java/nz/co/mircle/relationship/dao/PermissionRepositoryTest.java
@@ -0,0 +1,13 @@
+package nz.co.mircle.relationship.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by jacktan on 19/06/17.
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PermissionRepositoryTest {
+}
